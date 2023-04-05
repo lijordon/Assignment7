e5c6a1f383dfa706e5387f9ed760f9657cb3a373
Jordan Li <jueli@andrew.cmu.edu>
2023-04-05 22:12:02 +0900

    First Commit. Added Book and BookList class

---
+++ app/src/main/java/edu/temple/assignment7/Book.kt
@@ -0,0 +1,5 @@
+package edu.temple.assignment7
+
+data class Book (val title: String, val author: String) {
+
+}
---
+++ app/src/main/java/edu/temple/assignment7/BookList.kt
@@ -0,0 +1,21 @@
+package edu.temple.assignment7
+
+class BookList {
+    private val books: ArrayList<Book> = ArrayList()
+
+    fun add(book: Book) {
+        books.add(book)
+    }
+
+    fun remove(book: Book) {
+        books.remove(book)
+    }
+
+    fun get(index: Int): Book {
+        return books[index]
+    }
+
+    fun size(): Int {
+        return books.size
+    }
+}
