84f14fb4edebfc27011367346c4aeb005444c96d
Jordan Li <jueli@andrew.cmu.edu>
2023-04-05 22:21:14 +0900

    Added Books and initialization of books in oncreate

--- app/src/main/java/edu/temple/assignment7/MainActivity.kt
+++ app/src/main/java/edu/temple/assignment7/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var books: BookList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialises the booklist
+        books = BookList()
+
+        val bookTitles = resources.getStringArray(R.array.book_titles)
+        val bookAuthors = resources.getStringArray(R.array.book_authors)
+
+        for (i in bookTitles.indices) {
+            val book = Book(bookTitles[i], bookAuthors[i])
+            books.add(book)
+        }
+
     }
 }
